b77f551d7ce854bbc83378225a69159283ae7ba6
Will-cm <willcubamuriel@gmail.com>
2021-06-11 22:51:21 -0400

    update db connection

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url('mysql://us-cdbr-east-03.cleardb.com/heroku_5cdec0192cfaf15?reconnect=true');  ////
+
 return [
 
     /*
@@ -46,11 +48,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'], //env('DB_HOST', '127.0.0.1'),
+            'port' => $DATABASE_URL['port'], //env('DB_PORT', '3306'),
+            'database' => ltrim($DATABASE_URL['path'], "/") //env('DB_DATABASE', 'forge'),
+            'username' => $DATABASE_URL['user'], //env('DB_USERNAME', 'forge'),
+            'password' => $DATABASE_URL['pass'], //env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
