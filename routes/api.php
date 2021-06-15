f0e1e3af9ab50dd03a9f36a4e44bd344305dfdb5
Will-cm <willcubamuriel@gmail.com>
2021-06-15 01:43:02 -0400

    middleware

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });  */
 
 Route::group([
-    'middleware' => 'api',
+    //'middleware' => 'api',
     //'namespace' => 'App\Http\Controllers',
     'prefix' => 'auth'
 ], function () {
