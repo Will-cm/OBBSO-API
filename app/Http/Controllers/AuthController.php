11fa8b38bd6e725757ae52543dd4e10ead097cf3
Will-cm <willcubamuriel@gmail.com>
2021-06-15 11:13:08 -0400

    test login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
         $credentials = request(['username', 'password']);
 
         if (! $token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['error' => 'Unauthorized2'], 401);
         }
 
         return $this->respondWithToken($token);
