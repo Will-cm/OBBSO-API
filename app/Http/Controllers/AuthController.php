c96f971745929c7b52f28fde2054ec3a51f6e366
Will-cm <willcubamuriel@gmail.com>
2021-06-15 10:40:00 -0400

    test role2

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,16 +24,17 @@ class AuthController extends Controller
      */
     public function login()
     {
-      /*
+      
         $credentials = request(['username', 'password']);
 
         if (! $token = auth()->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
-        return $this->respondWithToken($token);*/
+        return $this->respondWithToken($token);
+        /*
         $clientes = User::all();    
-      return response()->json($clientes);
+      return response()->json($clientes);  */
     }
 
      /**
