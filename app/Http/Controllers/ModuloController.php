dad25a00f3dd2f85524c7eca2ed10e9b467cc34b
macut <macuto2142dt@gmail.com>
2021-06-28 21:15:40 -0400

    modules fixed ruta added

--- app/Http/Controllers/ModuloController.php
+++ app/Http/Controllers/ModuloController.php
@@ -61,7 +61,7 @@ class ModuloController extends Controller
       $modulo = Modulo::join("permisos","permisos.id_modulo","=","modulos.id_modulo")
                 ->where('permisos.id_user','=',$id)  //1 // auth()->user()
                 ->where('permisos.estado','=',1)
-                ->select("modulos.id_modulo","modulos.modulo", "modulos.vicon")
+                ->select("modulos.id_modulo","modulos.modulo", "modulos.vicon", "modulos.ruta")
                 ->get();
       return response()->json($modulo->toArray());
     }
